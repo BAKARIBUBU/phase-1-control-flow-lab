ead62c01d33c243ffed6467fe7347dba3ea91b2a
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-25 10:50:24 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let results
+ if (ride<=400){
+  results="This one is on me!"
+ }
+ else if (ride >400 && ride<2000){
+  results="That will be twenty bucks."
+ } else if (ride>=2000 && ride<=2500){
+  results='I will gladly take your thirty bucks.'
+ } else{
+  results="No can do."
+ }
+ return results
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  return city==="NYC"? 'Ok, sounds good.': 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+      default:
+        return 'Bye.'
+  }
 }
